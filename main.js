a0d52a49640a993e1364892e5328dc763edd168e
[Garcia Project] <[hgarcia3205@gmail.com]>
2026-02-13 18:11:59 -0600

    add first ever JS, and some CSS for future development

---
+++ main.js
@@ -0,0 +1,9 @@
+var firstVariable = 10;
+
+let secondVariable = 20;
+
+const thirdVariable = 50;
+
+let result = firstVariable + secondVariable + thirdVariable;
+
+console.log('sum of', firstVariable, '+', secondVariable, '+', thirdVariable, '=', result);
